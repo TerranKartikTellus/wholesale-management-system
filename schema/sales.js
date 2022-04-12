f0856b3671cf2efac3b4ad19d457f6698bdbaa85
Jaganath02 <workjaganath2002@gmail.com>
2022-04-12 10:48:53 +0530

    schema of sales and suppliers
    This is only schema

--- schema/sales.js
+++ schema/sales.js
@@ -1,12 +1,36 @@
 const mongoose = require("mongoose");
 
 const salesSchema = new mongoose.Schema({
-          cartArray:{ },
-          paymentVia:{ },
-          orderId:{ },
-          retailerId:{ },
-          timestamp:{ },
-          netCost:{ }
+          cartArray:{ 
+           },
+          paymentVia:{ 
+                 type: CharacterData, 
+                 required: [true, 'Please mention the payment method used'],
+                 unique: true,
+                 maxlength: [15, 'Please make sure its less than 15 charecters']
+          },
+          orderId:{
+                 type: Number, 
+                 required: [true, 'Please add a Order Id'],
+                 unique: true,
+                 maxlength: [10, 'Order id should be of 10 digit']
+           },
+          retailerId:{ 
+                 type: Number, 
+                 required: [true, 'Please add a Retailer Id'],
+                 unique: true,
+                 maxlength: [3, 'Retailer id should be of 3 digit']
+          },
+          timestamp:{
+                 type: timestamp, 
+                 required: ['Please add a timestamp'],
+          },
+          netCost:{  
+                 type: Number, 
+                 required: [true, 'Please add a Net cost'],
+                 unique: true,
+                 maxlength: [3, 'Product id should be of 3 digit']
+          }
 });
 
 module.exports = mongoose.models.Sales || mongoose.model('Sales', salesSchema);
