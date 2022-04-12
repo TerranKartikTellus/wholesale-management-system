375b847906c3ba22b820bcd1601ab0c131235030
Farhan Ansari <farhan.ansari1902@gmail.com>
2022-04-12 12:09:52 +0530

    Added schema for user

--- schema/user.js
+++ schema/user.js
@@ -1,12 +1,34 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-          uid:{ },
-          uFname:{ },
-          uLname:{ },
-          positionId:{ },
-          adminPrivilige:{ },
-          sex:{ }
+          uid:{type: Number,
+               required: [true, 'Please enter a valid ID'],
+               unique: true,
+               maxlength: [3, 'User ID should be of 3 digit']
+            },
+          uFname:{type: String,
+                  required: [true, 'Please enter First Name'],
+                  maxlength: 40,
+           },
+          uLname:{type: String,
+                  required: [true, 'Please enter Last Name'],
+                  maxlength: 40,
+
+           },
+          positionId:{type: Number,
+                      required: [true, 'Please enter a valid ID'],
+                      unique: true,
+                      maxlength: [3, 'Position ID should be of 3 digit']
+
+           },
+          adminPrivilige:{type: Character,
+                          maxlength: [1, 'Please enter Y/N'],
+                          required: [true, 'Please enter admin privilege']
+           },
+          sex:{type: Character,
+               maxlength: [1, 'Please enter m/f'],
+               required: [true, 'Please enter sex']
+           }
 });
 
 module.exports = mongoose.models.User || mongoose.model('User', userSchema);
