a785a6f3c89b4eb167f46dae839da6716145cd09
Jaganath02 <workjaganath2002@gmail.com>
2022-04-12 12:32:29 +0530

    finished
    check it out

--- pages/panel/categories/add.js
+++ pages/panel/categories/add.js
@@ -23,9 +23,10 @@ export default function Panel() {
                               </div>
                               <div className="p-20 bg-gray-100 mt-10 mx-10 bg-opacity-30">
                                       <div className="text-2xl tracking-wider">Add New Categories</div>
+                                      <br></br>
+                                      <div><Form></Form></div>
                               </div>
                     </div>
-                    <div><Form></Form></div>
           </div>
 );
 }
@@ -34,13 +35,13 @@ function Form(){
                 <>
                         <form>
                         <div class="form-group">
-                        <label for="exampleInputEmail1">Product category ID</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter ID"></input>
+                        <label for="123">Product category ID</label>
+                        <input type="number" class="form-control" id="123" placeholder="Enter ID"></input>
                         </div>
                         <br></br>
                         <div class="form-group">
-                        <label for="exampleInputPassword1">Product Category</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter The category"></input>
+                        <label for="charecterdata">Product Category</label>
+                        <input type="charecterdata" class="form-control" id="abc" placeholder="Enter The category"></input>
                         </div>
                         <button type="submit" className="bg-gray-800 text-gray-100 ml-20 p-3 rounded-md hover:bg-gray-100 hover:text-gray-900 font-semibold px-5 translate-y-16 hover:bg-opacity-30 tracking-wider" style={{position:'relative', right: '80px'}}>
                                 Submit
--- pages/panel/products/add.js
+++ pages/panel/products/add.js
@@ -24,8 +24,10 @@ export default function Panel() {
                               </div>
                               <div className="p-20 bg-gray-100 mt-10 mx-10 bg-opacity-30">
                                       <div className="text-2xl tracking-wider">Add New Product</div>
+                                      <br></br>
+                                      <div><Form></Form></div>
                               </div>
-                              <div><Form></Form></div>
+                              
                     </div>
           </div>
 );
@@ -35,38 +37,38 @@ function Form(){
                 <>
                         <form>
                         <div class="form-group">
-                        <label for="exampleInputEmail1">Product ID</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter ID"></input>
+                        <label for="123">Product ID</label>
+                        <input type="number" class="form-control" id="123" placeholder="Enter ID"></input>
                         </div>
                         <br></br>
                         <div class="form-group">
-                        <label for="exampleInputPassword1">Product Name</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Name"></input>
+                        <label for="name">Product Name</label>
+                        <input type="charecterdata" class="form-control" id="name"  placeholder="Enter Name"></input>
                         </div>
                         <br></br>
                         <div class="form-group">
-                        <label for="exampleInputPassword1">Maximum Retail Price</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter MRP"></input>
+                        <label for="123">Maximum Retail Price</label>
+                        <input type="number" class="form-control" id="123" placeholder="Enter MRP"></input>
                         </div>
                         <br></br>
                         <div class="form-group">
-                        <label for="exampleInputPassword1">Original Price</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Original price"></input>
+                        <label for="123">Original Price</label>
+                        <input type="number" class="form-control" id="123" placeholder="Enter Original price"></input>
                         </div>
                         <br></br>
                         <div class="form-group">
-                        <label for="exampleInputPassword1">Please suggest a quality rating for the product </label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Rating"></input>
+                        <label for="5">Please suggest a quality rating for the product </label>
+                        <input type="number" class="form-control" id="5" placeholder="Enter Rating"></input>
                         </div>
                         <br></br>
                         <div class="form-group">
-                        <label for="exampleInputPassword1">Quantity</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Please input the quantity "></input>
+                        <label for="123">Quantity</label>
+                        <input type="number" class="form-control" id="124" placeholder="Please input the quantity "></input>
                         </div>
                         <br></br>
                         <div class="form-group">
-                        <label for="exampleInputPassword1">Product category Id</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Id"></input>
+                        <label for="123">Product category Id</label>
+                        <input type="number" class="form-control" id="123" placeholder="Enter Id"></input>
                         </div>
                         <button type="submit" className="bg-gray-800 text-gray-100 ml-20 p-3 rounded-md hover:bg-gray-100 hover:text-gray-900 font-semibold px-5 translate-y-16 hover:bg-opacity-30 tracking-wider" style={{position:'relative', right: '80px'}}>
                                 Submit
