810f98866567d8f61dc4e29a555d05fef339ef84
Farhan Ansari <farhan.ansari1902@gmail.com>
2022-04-12 12:10:26 +0530

    Added schema for user position

--- schema/userPosition.js
+++ schema/userPosition.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const userPositionSchema = new mongoose.Schema({
-          positionID:{ },
-          positionName:{ }
+          positionID:{type: Number,
+                      required: [true, 'Please enter a valid ID'],
+                      unique: true,
+                      maxlength: [3, 'Position ID should be of 3 digit']
+
+           },
+          positionName:{type: String,
+                        required: [true, 'Please enter Position Name']
+           }
 });
 
 module.exports = mongoose.models.UserPosition || mongoose.model('UserPosition', userPositionSchema);
