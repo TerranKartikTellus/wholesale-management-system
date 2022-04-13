8b26c0c0b081ccdfbb453862cd9126ae137ad357
Jaganath02 <workjaganath2002@gmail.com>
2022-04-13 16:24:57 +0530

    Small changes in schema

--- schema/product.js
+++ schema/product.js
@@ -8,14 +8,40 @@ const productSchema = new mongoose.Schema({
                     maxlength: [3, 'Product ID should be of 3 digit']
           },
           pname:{ 
-                 type: String,
+            type: CharacterData, 
+            required: [true, 'Please mention the Product name'],
+            unique: true,
+            maxlength: [15, 'Please make sure its less than 15 charecters']
                     
            },
-          mrp:{ },
-          orignalPrice:{ },
-          rating:{ },
-          quantity:{ },
+          mrp:{ 
+            type: Number, 
+            required: [true, 'Please mention the MRP of the product'],
+            unique: true,
+            maxlength: [10, 'Decrease the the Zeros!']
+          },
+          orignalPrice:{
+            type: Number, 
+            required: [true, 'Please add the Original price'],
+            unique: true,
+            maxlength: [10, 'Price should be of 10 digit']
+           },
+          rating:{
+            type: Number, 
+            required: [true, 'Please add a Rating'],
+            unique: true,
+            maxlength: [2, 'Retailer id should be of 2 digits upto 10']
+           },
+          quantity:{ 
+            type: Number, 
+            required: [true, 'Please The quantity of product'],
+            unique: true,
+            maxlength: [3, 'Quantity should be of 3 digit']},
           productCategoryId:{ 
+            type: Number, 
+            required: [true, 'Please add a Product category id Id'],
+            unique: true,
+            maxlength: [3, 'The id should be of 3 digit']
 
            }
 });
--- schema/retailer.js
+++ schema/retailer.js
@@ -1,10 +1,30 @@
 const mongoose = require("mongoose");
 
 const retailerSchema = new mongoose.Schema({
-          rid:{  },
-          rname:{ },
-          address:{ },
-          contact:{ }
+          rid:{ 
+            type: Number, 
+            required: [true, 'Please add a Retailer Id'],
+            unique: true,
+            maxlength: [3, 'Retailer id should be of 3 digit']
+           },
+          rname:{
+            type: CharacterData, 
+            required: [true, 'Please mention the retailer name'],
+            unique: true,
+            maxlength: [15, 'Please make sure its less than 15 charecters'
+           },
+          address:{
+            type: CharacterData, 
+            required: [true, 'Please mention the retailer address'],
+            unique: true,
+            maxlength: [15, 'Please make sure its less than 15 charecters'
+           },
+          contact:{
+            type: Number, 
+            required: [true, 'Please add a contact number'],
+            unique: true,
+            maxlength: [10, 'Contact no should be of 10 digit']
+           }
 });
 
 module.exports = mongoose.models.Retailer || mongoose.model('Retailer', retailerSchema);
