07c6a39b4a746510e3bbf2f5d088daaeacb9e7de
Terran Kartik Tellus <tarrankartiktellus@gmail.com>
2022-04-19 20:37:59 +0530

    view,update,delete a user details

--- pages/panel/user/index.js
+++ pages/panel/user/index.js
@@ -21,7 +21,7 @@ return (
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
                     <div className="h-full bg-gray-200  w-3/12"><SidePanel></SidePanel></div>
-                    <div className="h-full bg-gradient-to-br from-purple-600/20 via-cyan-600/20 to-fuchsia-500/30 w-9/12">
+                    <div className="overflow-y-auto h-full bg-gradient-to-br from-purple-600/20 via-cyan-600/20 to-fuchsia-500/30 w-9/12">
                               <div className="flex flex-row items-center justify-between">
                               
                               <a href="/panel" className="bg-gray-800 text-gray-100 ml-20 p-3 rounded-md hover:bg-gray-100 hover:text-gray-900 font-semibold px-5 translate-y-16 hover:bg-opacity-30 tracking-wider">Back</a>
@@ -30,24 +30,24 @@ return (
                                         WholeSale Management <br></br> System
                               </div>
                               </div>
-                              <div className="mt-8 m-3 p-8">
-                                <div className="text-5xl font-mono font-bold">All users</div>
-                                <div><Table msg={router.query.newUser} users={AllUsers}></Table></div>
+                              <div className="mt-8 m-3 p-8 mx-auto">
+                                <div className="mx-auto text-5xl font-mono font-bold">All users</div>
+                                <div className="mx-auto "><Table msg={router.query.newUser} users={AllUsers}></Table></div>
                               </div>
                     </div>
                     
           </div>
 );
 }
-function Table({msg,users}){
+ function Table({msg,users}){
   const [newUser , setNewUser] = useState(msg);
-
+ 
   return(
-    <div className="">
+    <div className="mx-auto flex flex-row items-center justify-center mt-5">
       {
         users 
         ? 
-        <div className="flex flex-row overflow-x-auto">
+        <div className="">
           {
                                         newUser && 
                                         <div className="flex flex-row justify-between items-start w-96 p-2 tracking-wider m-1 rounded-lg absolute right-0 top-0 bg-green-600 text-gray-200">
@@ -61,21 +61,43 @@ function Table({msg,users}){
                                                 }
                                                 <div><button onClick={()=>{setNewUser("")}} className=" text-2xl bg-gray-900 p-2 ml-3 rounded-md">X</button></div>        
                                         </div>
-                                }
-          {users.map(i=>(
-            <div id={i._id} className="hover:scale-105 transition-all duration-300 ease-in-out flex flex-col justify-start items-start m-3 text-lg font-mono font-medium rounded-sm">
-              <div className="bg-cyan-800 w-56 p-1 rounded-t-sm text-gray-50">UID: {i.uid}</div>
-              <div className="bg-cyan-700 w-56 p-1 text-gray-50">First Name: {i.uFname}</div>
-              <div className="bg-cyan-600 w-56 p-1 text-gray-50">Last Name: {i.uLname}</div>
-              <div className="bg-cyan-500 w-56 p-1 ">Sex: {i.sex}</div>
-              <div className="bg-cyan-400 w-56 p-1 ">Position ID: {i.position}</div>
-              <div className="bg-cyan-300 w-56 p-1 ">Admin Privilige: {i.adminPrivilige}</div>
-              <div className="flex flex-row text-center  bg-cyan-300 w-56  rounded-b-sm">
-                <a href={`/api/operations/user/delete/{i.uid}`}  className="hover:text-lg transition-all duration-200 ease-in-out hover:text-gray-200 w-1/2 bg-gray-800 text-xl text-gray-100">Edit</a>
-                <a href={"/api/operations/user/edit/{i.uid}"}  className="hover:text-lg transition-all duration-200 ease-in-out hover:text-gray-200 w-1/2 bg-red-700 text-xl text-gray-100">Delete</a>
-              </div>
-            </div>
+          }
+          <div>
+            <table className="text-center px-2 py-1 "> 
+              <tr className="font-mono text-xl border-b-4 tracking-widest">
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">Sno</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">UID</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">First Name</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">Last Name</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">Position ID</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">Admin Privilige</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">Sex</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">Update</th>
+                <th className=" text-center px-2 py-1 bg-gray-700 text-gray-100">Delete</th>
+                
+              </tr>
+            
+          {users.map((i,x=1)=>(
+            <tr key={i.uid}  className="border-b-2 border-gray-800 tracking-wider bg-gray-700 bg-opacity-30 hover:bg-opacity-50">
+              <td className="text-lg  text-center px-2 py-1 bg-opacity-90 font-extrabold" >{x}</td>
+              <td className="text-lg  text-center px-2 py-1 bg-opacity-90 font-extrabold" >{i.uid}</td>
+              <td className="text-lg text-center px-2 py-1  bg-opacity-90" >{i.uFname}</td>
+              <td className="text-lg text-center px-2 py-1  bg-opacity-90" >{i.uLname}</td>
+              <td className="text-lg text-center px-2 py-1  bg-opacity-90" >{i.positionId}</td>
+              <td className="text-lg text-center px-2 py-1  bg-opacity-90" >{i.adminPrivilige}</td>
+              <td className="text-lg text-center px-2 py-1  bg-opacity-90" >{i.sex}</td>
+            
+              <td className="text-center px-2 py-1    bg-opacity-90 hover:bg-sky-600 transition-all duration-300 ease-in-out" >
+                <a href={`/panel/user/edit?id=${i._id}`}><img src="/edit.svg" className="w-7 h-7 mx-auto"></img></a>
+              </td>
+              <td className="text-center px-2 py-1    bg-opacity-90  hover:bg-red-600 transition-all duration-300 ease-in-out" >
+                <a href={`/panel/user/delete?id=${i._id}`}><img className="w-7 h-7 mx-auto" src="/delete.svg"></img></a>
+              </td>
+            
+            </tr>
           ))}
+          </table>
+          </div>
         </div> : <div className="text-3xl bg-gray-800 text-gray-100 ring-2 ring-red-600">No users found</div>
       }
     </div>
---
+++ pages/panel/user/update.js
@@ -0,0 +1,5 @@
+export default function update(){
+          return (
+                    <div>ss</div>
+          );
+}
